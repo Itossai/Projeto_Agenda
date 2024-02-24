f073be22d2e40018ea20a3861ade618737ff4b5a
Itossai <dantedeleon.ti@gmail.com>
2024-02-24 14:48:17 -0300

    adicionado Formulario de Registro de usuário

--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,11 @@ from contact import view
 app_name = 'contact'
 
 urlpatterns = [
+    # contact (CRUD)
     path('<int:contact_id>/', view.contact, name='contact'),
     path('search/', view.search, name='search'),
     path('', view.index, name='index'),  # type: ignore
+
+    # user
+    path('user/create/', view.register, name='register'),
 ]
