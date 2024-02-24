b3ee9b8a50fc7cace818b452560268e4a6ff58e7
Itossai <dantedeleon.ti@gmail.com>
2024-02-24 13:58:57 -0300

    adicionando pastas e arquivos para o usuário na agenda

---
+++ contact/view/user_forms.py
@@ -0,0 +1,5 @@
+from django.contrib import auth, messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+
+from contact.forms import RegisterForm
