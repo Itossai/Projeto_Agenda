3f85e9e10dd66c566a6648145d854b27e5770b40
Itossai <dantedeleon.ti@gmail.com>
2024-03-07 22:34:27 -0300

    atualizando labels, adicionando text input e texto de ajuda

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
+from . import models
+
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(
@@ -36,3 +38,33 @@ class RegisterForm(UserCreationForm):
                                 code='invalid')
             )
         return email
+
+
+class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Escreva seu nome aqui',
+            }
+        ),
+        label='Primeiro Nome',
+        help_text='Texto de ajuda para seu usuário',
+    )
+
+    class Meta:
+        model = models.Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
+
+    def clean(self):
+        # cleaned_data = self.cleaned_data
+
+        self.add_error('first_name', ValidationError(
+            'Mensagem de erro', code='invalid'))
+
+        self.add_error('last_name', ValidationError(
+            'Mensagem de error 2', code='invalid'
+        ))
+        return super().clean()
